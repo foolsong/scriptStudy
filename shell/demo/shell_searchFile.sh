6554914d1e3f4877d1d3744b5bdaba59f38966c7
宋永建 <ldusyj@163.com>
2017-02-27 16:37:11 +0800

    modify shellScript

--- shell/demo/shell_searchFile.sh
+++ shell/demo/shell_searchFile.sh
@@ -1,31 +1,15 @@
 #! /bin/sh  
-#测试echo、位置变量  
-# ls
-# echo hello
-# echo "\$* is $*"
-# echo "\$@ is $@"
-# echo "\$# is $#"
-# date
-
+cd ~ 
+cd /Users/songyongjian/Desktop
 arr=$(ls)
-temp=iOS
+temp='iOS'
 # echo $arr
 for i in $arr
 	do 
 	result=$(echo $i | grep "$temp")
+	# echo result = $result
 	if [[ "$result" != "" ]] 
 	then
 		echo $i
 	fi
 done
-
-
-# strA="long string"
-# strB="string"
-# result=$(echo $strA | grep "${strB}")
-# if [[ "$result" != "" ]]
-# then
-#     echo "包含"
-# else
-#     echo "不包含"
-# fi
